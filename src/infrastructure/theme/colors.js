16f7adb8786be3951dc650c5cc225cc8a404cb74
MD ABDUR RAHMAN <69203953+rahman163@users.noreply.github.com>
2022-01-23 05:52:10 +0800

    Files Upload

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,28 @@
+export const colors = {
+  brand: {
+    primary: '#2ad413',
+    secondary: '#5dc3c6',
+    muted: '#C6DAF7',
+  },
+  ui: {
+    primary: '#262626',
+    secondary: '#ff0000',
+    tertiary: '#F1F1F1',
+    quaternary: '#FFFFFF',
+    disabled: '#DEDEDE',
+    error: '#D0421B',
+    success: '#138000',
+  },
+  bg: {
+    primary: '#5cdafa',
+    secondary: '#baffea',
+  },
+  text: {
+    primary: '#262626',
+    secondary: '#757575',
+    disabled: '#9C9C9C',
+    inverse: '#FFFFFF',
+    error: '#D0421B',
+    success: '#b58d0b',
+  },
+};
